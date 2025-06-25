06609c33177a7f27668c7edb7183162110469081
Pawan Weerasuriya <uwpawan@gmail.com>
2025-06-26 03:25:19 +0530

    [SONA-19] counsellor dashboard placed nicely. this should be done in SONA-15, but processing it in there would be harder, so i did it here.

--- src/components/layout/Sidebar.tsx
+++ src/components/layout/Sidebar.tsx
@@ -25,7 +25,7 @@ interface SidebarProps {
 // Sidebar Component
 const Sidebar: React.FC<SidebarProps> = ({ isOpen, onClose }) => {
   const menuItems = [
-    { icon: Home, label: 'Home', href: '#' },
+    { icon: Home, label: 'Home', href: '/counsellor-dashboard' },
     { icon: Calendar, label: 'Sessions', href: '#' },
     { icon: Users, label: 'Clients', href: '#' },
     { icon: ThumbsUp, label: 'Feedbacks', href: '/counsellor-feedbacks' },
